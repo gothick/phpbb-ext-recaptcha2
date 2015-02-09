2292f4b27bc63633a3d6f7e425726affe82a708c
Matt Gibson <gothick@gothick.org.uk>
2015-02-09 23:05:53 +0000

    Reset the CAPTCHA configuration to something other than this extension's CAPTCHA when this extension is disabled!

--- ext.php
+++ ext.php
@@ -1,3 +1,46 @@
 <?php
-	// TODO: You do really need one of these. Check the noxwizard implementation
-?>
+/**
+ * Gothick New reCAPTCHA
+ *
+ * @package phpBB Extension - New reCAPTCHA
+ * @copyright (c) 2015 Matt Gibson Creative Ltd.
+ * @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+ *
+ */
+
+namespace gothick\newrecaptcha;
+
+class ext extends \phpbb\extension\base
+{
+	// override enable_step
+	function disable_step($old_state)
+	{
+		switch ($old_state)
+		{
+			case '':
+				// We need to change the CAPTCHA configuration back to something
+				// builtin if we're disabled when we're the active CAPTCHA, otherwise
+				// it'll confuse the board something chronic.
+
+				/* @var $config \phpbb\config\config */
+				$config = $this->container->get('config');
+
+				/* @var $newrecaptcha \gothick\newrecaptcha\captcha\recaptcha */
+				$newrecaptcha = $this->container->get('gothick.newrecaptcha.captcha.recaptcha');
+
+				if ($newrecaptcha->get_service_name() == $config['captcha_plugin'])
+				{
+					// Falling back to the built-in reCAPTCHA doesn't seem too
+					// presumptious.
+					$config->set('captcha_plugin', 'core.captcha.plugins.recaptcha');
+				}
+				return 'captcha_reset_to_core';
+			break;
+
+			default:
+				// Run parent disable step method
+				return parent::disable_step($old_state);
+			break;
+		}
+	}
+}
