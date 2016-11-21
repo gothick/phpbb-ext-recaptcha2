a96ee99be45631a07063c588005f50d980b0d7a7
Jens Møller <jens@jensz12.com>
2016-11-21 22:36:08 +0100

    Update captcha_recaptcha2.php

--- language/da/captcha_recaptcha2.php
+++ language/da/captcha_recaptcha2.php
@@ -23,17 +23,17 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'GOTHICK_RECAPTCHA2_LANG'					=> 'da', // Use https://developers.google.com/recaptcha/docs/language
-	'GOTHICK_RECAPTCHA2_NOT_AVAILABLE'			=> 'In order to use reCaptcha, you must create an account on <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
+	'GOTHICK_RECAPTCHA2_NOT_AVAILABLE'			=> 'For at du kan benytte reCaptcha, skal du først oprette en konto her <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
 	'GOTHICK_RECAPTCHA2'						=> 'reCaptcha 2.0',
 	// TODO: Hard to pin this down, as reCAPTCHA may have asked them to tick a box, enter a code, identify some pasta, so forth.
 	// For now, keep it general. Improve wording. Or see if Google passes back a less generic error message.
-	'GOTHICK_RECAPTCHA2_INCORRECT'				=> 'The reCAPTCHA you submitted was incorrect',
+	'GOTHICK_RECAPTCHA2_INCORRECT'				=> 'reCAPTCHAen du indtastede var ikke korrekt',
 
 	// Keys
 	'GOTHICK_RECAPTCHA2_SITEKEY'				=> 'reCaptcha site key',
-	'GOTHICK_RECAPTCHA2_SITEKEY_EXPLAIN'		=> 'Your public reCaptcha site key. Keys can be obtained on <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
+	'GOTHICK_RECAPTCHA2_SITEKEY_EXPLAIN'		=> 'Din offentlige reCaptcha site key. Nøgler kan findes her <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
 	'GOTHICK_RECAPTCHA2_SECRETKEY'				=> 'reCaptcha secret key',
-	'GOTHICK_RECAPTCHA2_SECRETKEY_EXPLAIN'		=> 'Your secret reCaptcha key. Keys can be obtained on <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
+	'GOTHICK_RECAPTCHA2_SECRETKEY_EXPLAIN'		=> 'Din hemmelige reCaptcha key. Nøgler kan findes her <a href="http://www.google.com/recaptcha">www.google.com/recaptcha</a>.',
 
 	'GOTHICK_RECAPTCHA2_CONFIRM_EXPLAIN'		=> 'To prevent automated registrations the board requires you to confirm that you are a human. If you are visually impaired or cannot otherwise complete the below task please contact the %sBoard Administrator%s.',
 	'GOTHICK_RECAPTCHA2_POST_CONFIRM_EXPLAIN' 	=> 'To prevent automated posts the board requires you to confirm that you are a human. If you are visually impaired or cannot otherwise complete the below task, please contact the %sBoard Administrator%s.',
